44e07ce9eae3539ddd32eea9d92033629192921d
nanic <131386211+nanic1@users.noreply.github.com>
2024-07-19 13:45:33 -0300

    Update ex06.py

--- Estrutura de Decisão/ex06.py
+++ Estrutura de Decisão/ex06.py
@@ -1,2 +1,15 @@
 def maior(x, y, z):
+    if x > y and x > z:
+        print(f'Maior: {x}')
+    elif y > x and y > z:
+        print(f'Maior: {y}')
+    elif z > x and z > y:
+        print(f'Maior: {z}')
+    else:
+        print('Todos são iguais.')
     
+num1 = int(input('Numero 1: '))
+num2 = int(input('Numero 2: '))
+num3 = int(input('Numero 3: '))
+
+maior(num1, num2, num3)
